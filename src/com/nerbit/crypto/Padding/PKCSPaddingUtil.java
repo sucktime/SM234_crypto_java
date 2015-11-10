09c47fc7ab51ac91c0dae604cb89a00c914ec771
sucktime <joooogoofooo@gmail.com>
2015-11-10 10:06:12 +0800

    added PKCS padding utils

--- src/com/nerbit/crypto/Padding/PKCSPaddingUtil.java
+++ src/com/nerbit/crypto/Padding/PKCSPaddingUtil.java
@@ -1,53 +1,53 @@
 package com.nerbit.crypto.Padding;
 
 public class PKCSPaddingUtil {
-	
-	public static byte[] PKCS7Padding(byte[] content, int blockSize) throws Exception{
-		if(content == null || blockSize < 8)
+
+	public static byte[] PKCS7Padding(byte[] content, int blockSize)
+			throws Exception {
+		if (content == null || blockSize < 8)
 			throw new Exception("parameter error");
-		
+
 		int len = content.length;
-		int remain = len%blockSize;
-		//if(remain == 0)
-			//remain = blockSize;
+		int remain = len % blockSize;
 		int NUM = blockSize - remain;
-		
-		int Len = blockSize*(len/blockSize+1);
+
+		int Len = blockSize * (len / blockSize + 1);
 		byte[] padded = new byte[Len];
 		System.arraycopy(content, 0, padded, 0, len);
-		for(int i=0; i < NUM ; i++)
-			padded[len+i] = (byte) NUM;
-		
+		for (int i = 0; i < NUM; i++)
+			padded[len + i] = (byte) NUM;
+
 		return padded;
 	}
-	
-	public static byte[] PKCS5Padding(byte[] content) throws Exception{
+
+	public static byte[] PKCS5Padding(byte[] content) throws Exception {
 		return PKCS7Padding(content, 8);
 	}
-	
-	public static byte[] PKCS7Unpadding(byte[] content, int blockSize) throws Exception{
-		if(blockSize < 8 || content == null || content.length%blockSize != 0)
+
+	public static byte[] PKCS7Unpadding(byte[] content, int blockSize)
+			throws Exception {
+		if (blockSize < 8 || content == null || content.length % blockSize != 0)
 			throw new Exception("parameter error");
 		return PKCS7Unpadding(content);
 	}
-	
-	public static byte[] PKCS7Unpadding(byte[] content) throws Exception{
-		if(content == null || content.length<8)
+
+	public static byte[] PKCS7Unpadding(byte[] content) throws Exception {
+		if (content == null || content.length < 8)
 			throw new Exception("parameter error");
-		
+
 		int len = content.length;
-		int NUM = content[len-1];
-		if(NUM>len)
+		int NUM = content[len - 1];
+		if (NUM > len)
 			throw new Exception("invalid padding");
-		for(int i=0; i<NUM; i++)
-			if(content[len-i-1] != NUM)
+		for (int i = 0; i < NUM; i++)
+			if (content[len - i - 1] != NUM)
 				throw new Exception("invalid padding");
-		byte[] unpadded = new  byte[len-NUM];
-		System.arraycopy(content, 0, unpadded, 0, len-NUM);
+		byte[] unpadded = new byte[len - NUM];
+		System.arraycopy(content, 0, unpadded, 0, len - NUM);
 		return unpadded;
 	}
-	
-	public static byte[] PKCS5Unpadding(byte[] content) throws Exception{
+
+	public static byte[] PKCS5Unpadding(byte[] content) throws Exception {
 		return PKCS7Unpadding(content, 8);
 	}
 }
--- src/com/nerbit/crypto/Padding/PaddingTest.java
+++ src/com/nerbit/crypto/Padding/PaddingTest.java
@@ -5,25 +5,23 @@ import java.security.SecureRandom;
 public class PaddingTest {
 	public static void main(String[] args) throws Exception {
 		/*
-		int i = 254;
-		byte b = (byte) i;
-		System.out.println(b);
-		*/
-		
-		//test padding:
+		 * int i = 254; byte b = (byte) i; System.out.println(b);
+		 */
+
+		// test padding:
 		byte[] b1 = new byte[8];
 		new SecureRandom().nextBytes(b1);
 		byte[] pb1 = PKCSPaddingUtil.PKCS7Padding(b1, 8);
-		for(int i=0; i<pb1.length; i++){
+		for (int i = 0; i < pb1.length; i++) {
 			System.out.print(pb1[i] + ",");
 		}
 		System.out.println();
-		
-		//test unpadding:
-		byte[] unpb1 = PKCSPaddingUtil.PKCS7Unpadding(pb1,8);
-		for(int i=0; i<unpb1.length; i++){
+
+		// test unpadding:
+		byte[] unpb1 = PKCSPaddingUtil.PKCS7Unpadding(pb1, 8);
+		for (int i = 0; i < unpb1.length; i++) {
 			System.out.print(unpb1[i] + ",");
 		}
-		
+
 	}
 }
